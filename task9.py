3f901dcf4bd49bc1ca454c1e5abcb3c3b7974b18
sese10 <109763742+sese10@users.noreply.github.com>
2022-10-06 13:22:33 +0200

    functions

---
+++ task9.py
@@ -0,0 +1,40 @@
+print("functions\n")
+# create a function that prints out "Hello World"
+# create a function that does nothing, an empty function
+# invoke/call both functions
+
+def greet():
+    print("Hello World")
+
+def age():
+    #pass      empty function
+    print(10)
+
+greet()
+age()
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def greet():
+    print("Hello World from python")
+
+def goodbye():
+    print("Thank you")
+    print("Good bye")
+
+def validate_username():     # empty function
+    pass
+
+greet()
+goodbye()
+validate_username() """
