1dcc23074dbc9cb3a5dcd4aae3f2708ded1350fa
sese10 <109763742+sese10@users.noreply.github.com>
2022-10-06 13:53:10 +0200

    Anonymous functions

---
+++ task10.py
@@ -0,0 +1,13 @@
+print ("anonymous function")
+#create an anonymous function that prints out "Hello World"
+#invoke/call the function
+
+def greet():
+    print("Hello World")
+
+# def accept(cb):
+    # cb("Hello")
+
+hello = lambda: print("Hello World")
+hello()
+# accept(lambda x: print(x))"""
